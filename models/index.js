2a31e73777c4a48ea4d342f240bebad2058002ca
Sathvik <satvikreddy2017@gmail.com>
2024-04-24 23:15:46 +0530

    Fixed model bug

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 require("dotenv").config({ path: `.env.${env}` });
-const config = require(__dirname + "/../config/config.js")[env];
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
